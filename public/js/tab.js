ac35d2acd263b2f15267ba4362579aaadbdf66ae
Andy Durette <duretteandy@gmail.com>
2020-06-14 22:21:06 -0400

    Resized componenets

--- public/js/tab.js
+++ public/js/tab.js
@@ -10,7 +10,7 @@ if (optionsTab.classList.contains("shifted") === false){
     optionsTab.getElementsByTagName('img')[0].src = "../images/right-arrow.svg";
 }else{  
     optionsTab.classList.remove("shifted"); 
-    optionsBar.style.right = "-261px";
+    optionsBar.style.right = "-201px";
     optionsTab.getElementsByTagName('img')[0].src = "../images/left-arrow.svg";
   }
 }
